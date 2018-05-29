cd67508e88ea6b6e338b24c989f3cb62b4768d5d
Avijit01 <37729351+Avijit01@users.noreply.github.com>
2018-05-29 21:57:31 +0600

    Add files via upload

---
+++ Problem_16.cpp
@@ -0,0 +1,42 @@
+/* A program to check an expression has either balanced parenthesis or not. */
+
+
+#include <iostream>
+#include<stack>
+
+using namespace std;
+
+stack<char>Str;
+
+
+int main() {
+
+
+    string Ex;
+
+    cout<<"Enter your Expression "<<endl;
+    cin>>Ex;
+
+    for (int i = 0; i < Ex.size() ; ++i) {
+
+        if(Ex[i] == '(') {
+            Str.push(Ex[i]);
+        }
+
+        else if(Ex[i] == ')'){
+            Str.pop();
+        }
+
+    }
+
+
+    if(Str.empty()){
+        cout<<"\nThe expression has balanced parenthesis " ;
+    }
+
+    else
+        cout<<"\nThe expression has not balanced parenthesis " ;
+
+
+    return 0;
+}
